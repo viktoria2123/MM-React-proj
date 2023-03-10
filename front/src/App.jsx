9597eed2dc7058fddadfe32dedc7d6a14406b95e
viktoria2123 <v.andreeva2123@gmail.com>
2023-03-11 00:32:32 +0100

    ccc

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-// import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-import Header from './src/common/header/Header'
+import Header from './common/header/Header'
+import Footer from './common/footer/Footer';
+
+import Home from './pages/home/Home'
+import Contact from './pages/contact/Contact'
+import Blog from './pages/blog/Blog'
+import About from './pages/about/About';
+import Article from './common/article/Article';
 
 import './index.scss';
 
@@ -77,7 +84,22 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+            <Header />
+
+<main>
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/contact' element={<Contact />} />
+    <Route path='/blog' element={<Blog />} />
+    <Route path='/about' element={<About />} />
+    <Route path='*' element={<h1>404 NOT FOUND</h1>} />
+
+
+  </Routes>
+  <Article />
+</main>
+
+<Footer />
       {step !== questions.length ? <Game step={step} question={question} onClickVariant={onClickVariant} />
       : (<Result correct={correct} />)
   }
--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+
 import App from './App';
-import Header from './common/header/Header';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
