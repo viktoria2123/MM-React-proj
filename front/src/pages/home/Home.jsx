e953bcb3c1c504db69dc46fffd18ed0f312e1b23
viktoria2123 <v.andreeva2123@gmail.com>
2023-03-18 23:02:20 -0400

    xc

--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -6,18 +6,29 @@ function Home() {
                 <img src="https://ca-times.brightspotcdn.com/dims4/default/cbfc74f/2147483647/strip/true/crop/3000x1871+0+0/resize/1200x748!/quality/80/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fa9%2F2c%2F97f345c32bbb0429783414e753ca%2Fa590172df48741379a91963e149d95d9" alt="" />
             </section>
             <section>
-                <h2>Life journey of Marilyn Monroe: from a difficult childhood to a star career</h2>
-                <p>Marilyn Monroe was born Norma Jeane Mortenson on June 1, 1926 in Los Angeles, California. Her childhood was marked by instability, as she was bounced around between different foster homes and relatives.</p>
+                <div class="card">
+                    <h2>Life journey of Marilyn Monroe: from a difficult childhood to a star career</h2>
+                    <p>Marilyn Monroe was born Norma Jeane Mortenson on June 1, 1926 in Los Angeles, California. Her childhood was marked by instability, as she was bounced around between different foster homes and relatives.
+<br />
+<br />
+<br />
 
-                <p>She married at the age of 16, but her husband was soon drafted into the military, leaving her alone and unemployed. In 1944, she began working in a factory that produced military aircraft, where a photographer discovered her and took photos for a modeling agency. This was the beginning of her modeling career, and eventually led to her first film contract in 1946.</p>
 
-                <p>Over the next several years, Marilyn starred in a number of successful films, including "Gentlemen Prefer Blondes," "The Seven Year Itch," and "Some Like It Hot." She became known for her beauty, talent, and captivating screen presence.</p>
 
-                <p>However, behind the scenes, Marilyn struggled with personal demons, including mental health issues, substance abuse, and a difficult love life. Despite her struggles, she remained dedicated to her craft and continued to make memorable contributions to film and popular culture.</p>
+                    </p>
+                </div>
 
-                <p>Marilyn's life was tragically cut short when she died of a drug overdose in 1962 at the age of 36. Despite her short life, she remains a cultural icon and continues to be celebrated for her beauty, talent, and contributions to the world of film.</p>
+                <div class="card">
+                    <h2>Modeling career and film success</h2>
+                    <p>She married at the age of 16, but her husband was soon drafted into the military, leaving her alone and unemployed. In 1944, she began working in a factory that produced military aircraft, where a photographer discovered her and took photos for a modeling agency. This was the beginning of her modeling career, and eventually led to her first film contract in 1946.</p>
+                </div>
 
+                <div class="card">
+                    <h2>Personal struggles and legacy</h2>
+                    <p>Despite her struggles, she remained dedicated to her craft and continued to make memorable contributions to film and popular culture. Marilyn's life was tragically cut short when she died of a drug overdose in 1962 at the age of 36. Despite her short life, she remains a cultural icon and continues to be celebrated for her beauty, talent, and contributions to the world of film.</p>
+                </div>
             </section>
+
         </>
     )
 }
